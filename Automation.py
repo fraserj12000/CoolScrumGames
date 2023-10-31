b04502fe10438c4d9908b7d7622606e3465203be
bkcalixto <89607788+bkcalixto@users.noreply.github.com>
2023-10-31 12:40:34 -0400

    Update Automation.py

--- Automation.py
+++ Automation.py
@@ -40,9 +40,20 @@ class Test(unittest.TestCase):
         self.navitems = self.driver.find_elements(By.XPATH, "/html/body/header/nav/div/div/ul/li")
         self.assertEqual(self.navitems[3].text, "Privacy")
     
-    def test_checkGame(self):
-        self.navitems = self.driver.find_elements(By.XPATH, "/html/body/header/nav/div/div/ul/li")
-        self.navitems[0].click()
+    def test_checkJakesGame(self):
+        self.jakesgame = self.driver.find_element(By.XPATH, "/html/body/div/main/div[2]/div[1]/h3/button")
+        self.jakesgame.click()
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+        self.playbutton = self.driver.find_element(By.XPATH, "/html/body/div/main/button")
+        self.assertEqual(self.playbutton.text, "Play Game")
+        self.playbutton.click()
+        self.canvas = self.driver.find_elements(By.XPATH, '//*[@id="gameCanvas"]')
+        assert self.canvas[0] is not None
+
+    def test_checkSnakeGame(self):
+        self.snakegame = self.driver.find_element(By.XPATH, "/html/body/div/main/div[2]/div[2]/h3/button")
+        self.snakegame.click()
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(2)
         self.playbutton = self.driver.find_element(By.XPATH, "/html/body/div/main/button")
