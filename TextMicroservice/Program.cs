16da7a40d1281e9acb649f87a58ac26c4e8650c3
starterpack813 <83788094+starterpack813@users.noreply.github.com>
2023-11-10 19:24:03 -0500

    Update

--- TextMicroservice/Program.cs
+++ TextMicroservice/Program.cs
@@ -1,24 +1,56 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Cors;
+using System.Net.Sockets;
+using System.Net;
 
-namespace TextMicroservice
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
+
+var app = builder.Build(); //This page is what actually creates the API endpoint with the text
+
+//The CORS allows the request to actually get through to the API endpoint. No idea why it's necessary but it didn't work without it.
+app.UseCors(builder =>
 {
-    public class Program
+    builder
+        .AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+});
+
+
+
+string getText() //This method is what pulls the data from the text file.
+{
+    string text = "";
+    try
     {
-        public static void Main(string[] args)
-        {
-            // creates a host, builds it, and runs it
-            CreateHostBuilder(args).Build().Run();
-
-        }
-
-        //responsible for configuring the host and setting up the web server
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<TextMicroservice.Startup>();
-                });
+        // Sets the path to the text file that you want to read
+        string filePath = "AboutUs.txt";
+        // Reads the content of the text file
+        text = System.IO.File.ReadAllText(filePath);
     }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error: " + ex.Message);
+    }
+    return text;
 }
 
+app.MapGet("/AboutUs", () =>
+{
+    var text = new AboutUsText(getText());
+    return text;
+})
+.WithName("GetAboutUs");
+
+app.Run();
+
+
+internal record AboutUsText(string text)
+{
+    string t = text;
+}
